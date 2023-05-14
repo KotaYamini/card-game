2c51d958db1c454289d062f306677f1fcec6ef1f
yaminikota <yamini.kota466@gmail.com>
2023-05-14 23:14:47 +0530

    deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatchingGameComponent } from './matching-game/matching-game.component';
 import { Game1Component } from './game1/game1.component';
-import { Game2Component } from './game2/game2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MatchingGameComponent,
     Game1Component,
-    Game2Component
   ],
   imports: [
     BrowserModule,
